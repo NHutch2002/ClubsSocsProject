05bd36d5a227b248652b93fcc40ccfa154ca2820
Nathan Hutchison <2556961h@student.gla.ac.uk>
2022-03-05 19:31:18 +0000

    Base template complete with index.html

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+
+    context_dict = {}
+
+    response = render(request, 'rango/index.html', context_dict)
+
+    return response
